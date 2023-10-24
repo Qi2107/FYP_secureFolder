a57ff9edddd2d949169d7d27cd5b932a6a1635f8
Kaneighki <84215782+Kaneighki@users.noreply.github.com>
2023-10-24 20:56:25 +0800

    Add files via upload
    Added back forget password button, sends password reset email

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,7 +2,7 @@ import { useNavigation } from '@react-navigation/core';
 import React, { useEffect, useState } from 'react';
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, Platform } from 'react-native'
 import { auth } from '../firebase';
-import {signInWithEmailAndPassword} from 'firebase/auth';
+import {signInWithEmailAndPassword, sendPasswordResetEmail} from 'firebase/auth';
 
 
 const LoginScreen = () => {
@@ -29,6 +29,15 @@ const LoginScreen = () => {
         console.log('Logged in with:', user.email);
       })
       .catch(error => alert(error.message))
+  }
+
+  const ForgetPassword = () => {
+    sendPasswordResetEmail(auth, email)
+    .then(() => {
+      alert("Reset email has been sent")
+    }).catch((error) =>{
+      alert("Error")
+    })
   }
 
   return (
@@ -58,6 +67,13 @@ const LoginScreen = () => {
           style={styles.button}
         >
           <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={ForgetPassword}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Forget Password?</Text>
         </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
